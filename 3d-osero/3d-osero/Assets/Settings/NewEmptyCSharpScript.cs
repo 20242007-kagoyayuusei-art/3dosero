2d5050da0be18c13fc8c2010d447cdbe0d41afdc
TT940283 <20242015-takedatomoharu@hcs.ac.jp>
2025-12-03 10:09:01 +0900

    c#settei

---
+++ 3d-osero/3d-osero/Assets/Settings/NewEmptyCSharpScript.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+#public class NewEmptyCSharpScript
+#{
+    
+#}
+public class Player : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Hello from C#!");
+    }
+}
